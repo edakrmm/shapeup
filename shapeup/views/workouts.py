b15df680f38b1cb8c2b59ac59e38dc81364241e2
EDA KARAMEMISOGLU <TCEKARAMEMISOGLU@turkcell.entp.tgc>
2021-11-28 14:48:13 +0300

    workout sayfasının loglansması için düzenleme yapıldı

--- shapeup/views/workouts.py
+++ shapeup/views/workouts.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 import logging
 
 logger = logging.getLogger('workouts_displayed')
 
+@login_required(login_url='login')
 def workouts(request):        
 
     if request.user.is_authenticated:
