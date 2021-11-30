9fe489c053ebb4c7e77072acbfca5f5c1fc7e5b5
EDA KARAMEMISOGLU <TCEKARAMEMISOGLU@turkcell.entp.tgc>
2021-11-30 21:17:58 +0300

    production için postgresql ayarları yapıldı

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,12 +1,27 @@
 from .base import *
 
-ALLOWED_HOSTS = ['www.edakaramemisoglu.com']
+ALLOWED_HOSTS = ['edakaramemisoglu.com','127.0.0.1','localhost', '3.19.14.123']
 
 DEBUG = False
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -22,3 +37,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AWS_ACCESS_KEY_ID = env("ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "edakaramemisoglus3"
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400"
+}
+
+AWS_LOCATION = 'static'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+DEFAULT_FILE_STORAGE = 'config.storage_backend.MediaStorage'
